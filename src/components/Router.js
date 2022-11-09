ca3e6c33d742991fea25477078a12e3aa9b3d06e
박채린 <chaerin@bagchaelin-ui-MacBookAir.local>
2022-11-09 20:16:04 +0900

    chaerin-pages update

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Pages
 import HistoryEdit from "../pages/HistoryEdit";
 import HistoryView from "../pages/HistoryView";
-
+import Home from '../pages/Home';
+import Help from '../pages/Help';
+import Navbar from './Navbar';
 const Router = ({ user }) => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="/login" element={<div>카카오톡로그인</div>} />
+        <Route path="/map" element={<div>map</div>} />
+        <Route path="/help" element={<Help/>} />
         <Route path="/history/view" element={<HistoryView user={user} />} />
         <Route path="/history/edit" element={<HistoryEdit user={user} />} />
+
       </Routes>
     </BrowserRouter>
   );
