eb78001811fc2585732992cea5c6bc5a78aa2ed3
dianexjeong <zuphyr14@gmail.com>
2022-11-21 22:43:58 +0900

    feat: add member modal

---
+++ src/components/Member.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+const Member = (props) => {
+  const removeUser = () => {
+    console.removeUser("hi");
+  };
+  return (
+    <div>
+      <MemberBox>
+        {props.name}
+        <XButton onClick={removeUser}>&times;</XButton>
+      </MemberBox>
+    </div>
+  );
+};
+
+export default Member;
+
+const MemberBox = styled.section`
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  background: #ffffff;
+  padding: 10px 10px 10px 10px;
+  margin-bottom: 10px;
+  border: 1px solid #e2e2e2;
+  box-shadow: 1px 3px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  font-family: "Spoqa Han Sans Neo", "sans-serif";
+`;
+
+const XButton = styled.button`
+  outline: none;
+  background-color: transparent;
+  color: #8d4bf6;
+  text-align: right;
+`;
