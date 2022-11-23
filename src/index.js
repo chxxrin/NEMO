08d0376616ae6fe0eaa3e223b25d36a30b5fd043
keepgoing-hyeonsoo <chs98105@gmail.com>
2022-11-24 08:26:10 +0900

    [etc]: package.json 에서 REACT 환경변수 설정

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import axios from 'axios'
 
 import { AuthProvider } from './contexts/AuthContext'
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.REACT_APP_ENV === 'development') {
   // yarn start (개발용 API 서버)
   axios.defaults.baseURL = process.env.REACT_APP_API_URL_DEV
 } else {
   // yarn build (배포용 API 서버)
   axios.defaults.baseURL = process.env.REACT_APP_API_URL_PROD
 }
+
+console.log('axios.defaults.baseURL : ', axios.defaults.baseURL)
 
 axios.defaults.timeout = 5000
 
