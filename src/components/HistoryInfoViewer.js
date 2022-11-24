7523ffb9d8aee5247560b54acf6dd0607633c7f8
dianexjeong <zuphyr14@gmail.com>
2022-11-25 02:38:23 +0900

    feat: view history list

--- src/components/HistoryInfoViewer.js
+++ src/components/HistoryInfoViewer.js
@@ -2,23 +2,46 @@ import React, { useReducer } from "react";
 import styled from "styled-components";
 import DateSelect from "./DateSelect";
 
-const HistoryInfoViewer = ({ historyObj }) => {
-  return (
-    <div className="info-wrapper">
-      <section id="title">
-        <h5
-          style={{ marginTop: "5px", marginBottom: "10px", fontWeight: "bold" }}
-        >
-          {historyObj.history.title}
-        </h5>
-        <Info className="info">
-          {historyObj.studio.company}
-          {historyObj.studio.name + " "}
-          {historyObj.history.history_date}
-        </Info>
-      </section>
-    </div>
-  );
+const HistoryInfoViewer = ({ historyObj, trace }) => {
+  if (trace === "list") {
+    return (
+      <div className="info-wrapper">
+        <section id="title">
+          <h5
+            style={{
+              marginTop: "5px",
+              marginBottom: "10px",
+              fontWeight: "bold",
+            }}
+          >
+            {historyObj.history.title}
+          </h5>
+          <Info className="info">{historyObj.history.history_date}</Info>
+        </section>
+      </div>
+    );
+  } else {
+    return (
+      <div className="info-wrapper">
+        <section id="title">
+          <h5
+            style={{
+              marginTop: "5px",
+              marginBottom: "10px",
+              fontWeight: "bold",
+            }}
+          >
+            {historyObj.history.title}
+          </h5>
+          <Info className="info">
+            {historyObj.studio.company}
+            {historyObj.studio.name + " "}
+            {historyObj.history.history_date}
+          </Info>
+        </section>
+      </div>
+    );
+  }
 };
 
 export default HistoryInfoViewer;
--- src/pages/HistoryView.js
+++ src/pages/HistoryView.js
@@ -1,65 +1,65 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import History from '../components/History'
-import HistoryImageViewer from '../components/HistoryImageViewer'
-import HistoryTop from '../components/HistoryTop'
-import MemberModal from '../components/MemberModal'
-import styled from 'styled-components'
-import NavbarNone from '../components/NavbarNone'
-import axios from 'axios'
-import '../css/History.css'
-import '../css/Navbar.css'
-import { useLocation } from 'react-router-dom'
-import HistoryInfoViewer from '../components/HistoryInfoViewer'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import History from "../components/History";
+import HistoryImageViewer from "../components/HistoryImageViewer";
+import HistoryTop from "../components/HistoryTop";
+import MemberModal from "../components/MemberModal";
+import styled from "styled-components";
+import NavbarNone from "../components/NavbarNone";
+import axios from "axios";
+import "../css/History.css";
+import "../css/Navbar.css";
+import { useLocation } from "react-router-dom";
+import HistoryInfoViewer from "../components/HistoryInfoViewer";
 
 const HistoryView = () => {
-  const [histories, setHistories] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const [histories, setHistories] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const location = useLocation()
-  const hashed_history_id = location.pathname.split('/')[3]
+  const location = useLocation();
+  const hashed_history_id = location.pathname.split("/")[3];
 
   useEffect(() => {
     const fetchHistories = async () => {
       try {
-        setError(null)
-        setHistories(null)
-        setLoading(true)
+        setError(null);
+        setHistories(null);
+        setLoading(true);
         const response = await axios
           .get(
             `history/hashed/${hashed_history_id}`,
             {
-              headers: { 'Access-Control-Allow-Origin': '*' },
+              headers: { "Access-Control-Allow-Origin": "*" },
             },
             { withCredentials: true }
           )
           .then((res) => {
-            console.log(res.data) // 데이터 잘 가공해주셈!!
-            setHistories(res.data)
-          })
+            console.log(res.data); // 데이터 잘 가공해주셈!!
+            setHistories(res.data);
+          });
       } catch (e) {
-        setError(e)
-        console.log(e)
+        setError(e);
+        console.log(e);
       }
-      setLoading(false)
-    }
-    fetchHistories()
-  }, [])
+      setLoading(false);
+    };
+    fetchHistories();
+  }, []);
 
-  const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
 
   const openModal = () => {
-    setModalOpen(true)
-  }
+    setModalOpen(true);
+  };
   const closeModal = () => {
-    setModalOpen(false)
-  }
+    setModalOpen(false);
+  };
 
-  if (loading) return <div>로딩중</div>
-  if (error) return <div>에러</div>
-  if (!histories) return <div>no histories</div>
-
+  if (loading) return <div>로딩중</div>;
+  if (error) return <div>에러</div>;
+  if (!histories) return <div>no histories</div>;
+  console.log(histories);
   return (
     <div className="container">
       <NavbarNone />
@@ -67,10 +67,10 @@ const HistoryView = () => {
       <HistoryInfoViewer historyObj={histories} />
       <HistoryImageViewer historyObj={histories} />
     </div>
-  )
-}
+  );
+};
 
-export default HistoryView
+export default HistoryView;
 
 const BtnPurple = styled.button`
   border: none;
@@ -81,4 +81,4 @@ const BtnPurple = styled.button`
   background-color: #8861c2;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
   border-radius: 20px;
-`
+`;
