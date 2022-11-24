676607cb021fe43ef39f558c44b06921cef8b3ac
박채린 <chaerin@bagchaelin-ui-MacBookAir.local>
2022-11-24 21:14:43 +0900

    update

--- src/components/NavbarMap.js
+++ src/components/NavbarMap.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import KakaoLoginBtn from "./KakaoLoginBtn";
 import KakaoLoginNavBtn from "./KakaoLoginNavBtn";
+import axios from 'axios';
+import MapHis from "../pages/MapHis";
 
 // ICONS
 import * as FaIcons from "react-icons/fa"; //Now i get access to all the icons
@@ -21,10 +23,14 @@ import { SidebarData } from "./SlidebarData";
 // STYLES
 import "../css/Navbar.css";
 import "../css/Map.css";
+import "../css/NavbarMap.css";
+
 import { BsMusicNote, BsMusicNoteList } from "react-icons/bs";
 
 // Context API
 import { useAuthContext } from "../contexts/AuthContext";
+
+const API = process.env.REACT_APP_API_URL_PROD;
 
 export default function NavbarMap({ parentFunction }) {
   const { isAuth, userData, logout } = useAuthContext();
@@ -43,16 +49,78 @@ export default function NavbarMap({ parentFunction }) {
   };
   let [search, setSearch] = useState("");
 
+
+  const [studios, setStudios] = useState([])  
+  const [query, setQuery] = useState(null)
+  useEffect(() => {
+      let completed = false;
+
+      async function get() {
+          const result = await axios(API + `studio/?search=${query}`)
+          if(!completed) {
+              setStudios(result.data);
+          }
+      }
+      get()
+      return () => {
+          completed = true
+      }
+  }, [query])
+
+
+
+
+
+
+  let [diff, setDiff] = useState(0); // 마커 인덱스 구분하기 위한 state 변수
+  let [index, setIndex] = useState(0); // all 마커 인덱스 구분하기 위한 state 변수
+  let [modal, setModal] = useState(false);
+  let [trick, setTrick] = useState(0);
+  let [markers, setMarkers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  let [result, setResult] = useState(null);
+  let [flag, setFlag] = useState(false);
+  let [storeresult, setStoreresult] = useState(0);
+
+  const parentGetmarkerIndex = (x) => {
+    setIndex(x);
+    console.log(index);
+    setFlag(true);
+    console.log(flag);
+    //const URL = API + "/studio/"+x+"/";
+
+    const onestudio = async () => {
+      try {
+        setError(null);
+        //setStoreresult(null);
+        setLoading(true);
+        const response = await axios.get("studio/" + x);
+        setStoreresult(response.data);
+        console.log(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    onestudio();
+  };
+
+
+
+
+  
+
   return (
     <>
       <IconContext.Provider value={{ color: "#8861c2" }}>
-        {/* All the icons now are white */}
+
         <div className="navbar">
           <Link to="#" className="map-menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
 
-          <div className="SearchContainer">
+          {/* <div className="SearchContainer">
             <div className="SearchBar">
               <input
                 className="SearchInput"
@@ -70,7 +138,45 @@ export default function NavbarMap({ parentFunction }) {
                 <AiIcons.AiOutlineSearch />
               </button>
             </div>
-          </div>
+          </div> */}
+
+
+            <div className="SearchBar">
+
+            <input className="SearchInput"
+                    value={query}
+                    onChange={e => setQuery(e.target.value)} />
+            </div>
+  
+
+        <div className="SearchOutput">
+            <div>
+                {studios.map((studio) => (
+                <div key={studio.id} className="output-border">
+
+                  <button
+                    className="Search-btn"
+                    onClick={(value) => {
+                    navigate(`/maphis/${studio.id}`, {
+                    state: { storeresult: studio },
+                    });
+                    }}
+                    >
+                    {studio.company} {studio.name}<br/>
+                    ({studio.address})
+                    </button>
+
+                </div>
+                ))}
+            </div>
+        </div>
+
+
+
+
+
+
+
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>
